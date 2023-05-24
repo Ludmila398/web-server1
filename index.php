ad31af6f8c5505fc793f309109a73c32c103ceea
Liudmila <liudmilagubanova18@gmail.com>
2023-05-24 13:22:46 +0400

    add changes

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
 // Печатает год в STDOUT
+print_r ($_SERVER);
 echo date('Y');
 echo "\n";
 echo 'Hello, world!';
 echo "\n";
 print_r("rrrrrrrrrrr \n" );
 print_r("rnnnnnnnnn \n" );
+print_r("\n");
+print_r ($_GET);
+?>
+
+new text
+new new
+
+<h1>hello, php!</h1>
+
+
+<h1> <?= 'i am from PHP!'?> </h1>
